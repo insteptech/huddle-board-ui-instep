35975e3ca52963c09bbae6696edb0fa314907715
Gagandeep Singh <gagandeep.singh@gramenerit.com>
2024-04-19 12:34:02 +0530

    design added

---
+++ .next/server/_rsc_app_components_footer_index_tsx.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_app_components_footer_index_tsx";
+exports.ids = ["_rsc_app_components_footer_index_tsx"];
+exports.modules = {
+
+/***/ "(rsc)/./app/components/footer/index.tsx":
+/*!*****************************************!*\
+  !*** ./app/components/footer/index.tsx ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst Footer = ()=>{\n    return \"Footer\";\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Footer);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9hcHAvY29tcG9uZW50cy9mb290ZXIvaW5kZXgudHN4IiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxNQUFNQSxTQUFTO0lBQ2IsT0FBTztBQUNUO0FBRUEsaUVBQWVBLE1BQU1BLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kb2N0dXN0ZWNoLy4vYXBwL2NvbXBvbmVudHMvZm9vdGVyL2luZGV4LnRzeD8wY2VjIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IEZvb3RlciA9ICgpID0+IHtcbiAgcmV0dXJuICdGb290ZXInO1xufTtcblxuZXhwb3J0IGRlZmF1bHQgRm9vdGVyO1xuIl0sIm5hbWVzIjpbIkZvb3RlciJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(rsc)/./app/components/footer/index.tsx\n");
+
+/***/ })
+
+};
+;
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9KtqxhfvwRMXGq532zn5PMcabPitm4/FrOKuhZvrnpw=\"\n}"
